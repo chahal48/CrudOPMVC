877aa5109ea20b86bb85ab413d6c211d679c6da8
chahal48 <sachin.chahal48@gmail.com>
2022-12-18 09:54:07 +0530

    Working 16

---
+++ CrudOPMVC/Models/MaximumAgeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudOPMVC.Models
+{
+    public class MaximumAgeAttribute : ValidationAttribute
+    {
+        int _MaximumAge;
+
+        public MaximumAgeAttribute(int maximumAge)
+        {
+            _MaximumAge = maximumAge * -1;
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(Convert.ToString(value), out date))
+            {
+                return date > DateTime.Now.AddYears(_MaximumAge);
+            }
+            return false;
+        }
+    }
+}
---
+++ CrudOPMVC/Models/MinimumAgeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudOPMVC.Models
+{
+    public class MinimumAgeAttribute : ValidationAttribute
+    {
+        int _minimumAge;
+
+        public MinimumAgeAttribute(int minimumAge)
+        {
+            _minimumAge = minimumAge * -1;
+        }
+
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(Convert.ToString(value), out date))
+            {
+                return date < DateTime.Now.AddYears(_minimumAge);
+            }
+            return false;
+        }
+    }
+}
